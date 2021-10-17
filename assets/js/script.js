cba98f1e690648f462e7a1bec74891f1869cd83e
Rakib Ahmed <rakib987@gmail.com>
2021-10-17 19:53:03 -0400

    initial app interface completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 console.log(`JS is Connected`);
 
-var gameButton = document.querySelector(`.newButton`);
+var gameButton = document.querySelector(`.gameButton`);
 gameButton.addEventListener(`click`, event => {
     // Execute code below
     gameButton.innerHTML++;
