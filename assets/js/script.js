1df7afc51007c0ae88b98ac32ba4ddbbba272d12
Rakib Ahmed <rakib987@gmail.com>
2021-10-17 20:24:53 -0400

    10/17 added a bit of game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
-console.log(`JS is Connected`);
+// Declaring Game Variables
+const gameContent = $(`.gameContent`);
+const startGameButton = $(`#startGame`);
 
-var gameButton = document.querySelector(`.gameButton`);
-gameButton.addEventListener(`click`, event => {
-    // Execute code below
-    gameButton.innerHTML++;
+// Array
+const selections = [
+    {
+        name: `Rock`,
+        beats: `Scissors`
+    },
+    {
+        name: `Paper`,
+        beats: `Rock`
+    },
+    {
+        name: `Scissors`,
+        beats: `Paper`
+    }
+];
+
+// Functionality
+gameContent.hide();
+startGameButton.on(`click`, event => {
+    gameContent.show(500);
+    let randomChoice = Math.floor(Math.random() * selections.length);
+    const computerChoice = selections[randomChoice].name;
+    $(`.computerSelection`).html(computerChoice);
 })
